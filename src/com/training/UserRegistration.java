cad683f2d2cd7aa4fbb74ae2dc7f47ac960738df
Yudha13Koley <yudhajit1998@gmail.com>
2020-09-26 15:28:31 +0530

    UC1 done

--- src/com/training/UserRegistration.java
+++ src/com/training/UserRegistration.java
@@ -1,8 +1,38 @@
 package com.training;
 
+import java.util.regex.*;
+
 public class UserRegistration {
+	private Pattern pattern;
+	private Matcher matcher;
+	private String firstName;
+	private String firstNamePattern="^[A-Z][a-z]{3,}";
+	public UserRegistration() {
+		pattern=Pattern.compile(firstNamePattern);
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		if(this.validateFirstName(firstName))
+		this.firstName = firstName;
+		else
+			System.out.println("First Name is Invalid !");
+	}
+	public boolean validateFirstName(String fistname) {
+		matcher=pattern.matcher(fistname);
+		return matcher.matches();
+	}
+	@Override
+	public String toString() {
+		return "FirstName = " + firstName;
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to User Registration Problem !");
+		UserRegistration UR=new UserRegistration();
+		UR.setFirstName("Yudha");
+		System.out.println(UR);
 	}
 
 }
